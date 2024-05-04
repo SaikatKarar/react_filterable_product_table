d081e03cdf1cc95916575911bfd4e913096e1fae
SaikatKarar <kararsaikat@gmail.com>
2024-05-04 09:57:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import FilterableProductTable from './components/FilterableProductTable';
+
+function App() {
+
+  return (
+    <>
+      <FilterableProductTable />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FilterableProductTable() {
+    return (
+        <div>FilterableProductTable</div>
+    )
+}
+
+export default FilterableProductTable
